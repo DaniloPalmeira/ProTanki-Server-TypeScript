82a0f9d2885eb82c3b02e3c8f2aa226aa1620b0b
Danilo Palmeira <124080087+DaniloPalmeira@users.noreply.github.com>
2025-06-27 01:56:28 -0300

    feat: Adiciona novos recursos de mapa ao fluxo de login

--- src/workflows/LoginWorkflow.ts
+++ src/workflows/LoginWorkflow.ts
@@ -21,7 +21,7 @@ export class LoginWorkflow {
     client.sendPacket(new SocialNetwork(server.getSocialNetworks()));
     client.sendPacket(new CaptchaLocation(server.configService.getCaptchaLocations()));
 
-    const resourceIds: ResourceId[] = ["ui/language_images", "ui/login_background", "ui/quests/icons/battle_score", "ui/quests/icons/get_crystal", "ui/quests/icons/kill_enemies", "ui/quests/window/week1/quest_chain", "ui/quests/window/week2/quest_chain", "ui/quests/window/week3/quest_chain", "ui/quests/window/week4/quest_chain", "ui/quests/window/week1/final_reward", "ui/quests/window/week2/final_reward", "ui/quests/window/week3/final_reward", "ui/quests/window/week4/final_reward"];
+    const resourceIds: ResourceId[] = ["ui/language_images", "ui/login_background", "ui/quests/icons/battle_score", "ui/quests/icons/get_crystal", "ui/quests/icons/kill_enemies", "ui/quests/window/week1/quest_chain", "ui/quests/window/week2/quest_chain", "ui/quests/window/week3/quest_chain", "ui/quests/window/week4/quest_chain", "ui/quests/window/week1/final_reward", "ui/quests/window/week2/final_reward", "ui/quests/window/week3/final_reward", "ui/quests/window/week4/final_reward", "maps/map_sandbox/preview/SUMMER", "maps/map_sandbox/preview/WINTER", "maps/map_sandbox/preview/SUMMER_NIGHT", "maps/map_sandbox/preview/SPACE", "maps/map_sandal/preview/SUMMER", "maps/map_sandal/preview/WINTER", "maps/map_sandal/preview/SPACE", "maps/map_serpuhov/preview/SUMMER", "maps/map_serpuhov/preview/WINTER", "maps/map_serpuhov/preview/SPACE", "sounds/notifications/battle_invite"];
 
     const dependencies = {
       resources: ResourceManager.getBulkResources(resourceIds),
