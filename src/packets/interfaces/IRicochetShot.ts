67a6da545b0e73e32d08b47671b472b2abf0d8f9
Danilo Palmeira <124080087+DaniloPalmeira@users.noreply.github.com>
2025-07-17 22:30:24 -0300

    feat: Adiciona pacotes e handlers para o disparo da arma Ricochete

---
+++ src/packets/interfaces/IRicochetShot.ts
@@ -0,0 +1,17 @@
+import { IPacket } from "./IPacket";
+
+export interface IRicochetShotCommand extends IPacket {
+    clientTime: number;
+    shortId: number;
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface IRicochetShotPacketData {
+    nickname: string | null;
+    x: number;
+    y: number;
+    z: number;
+}
+export interface IRicochetShotPacket extends IPacket, IRicochetShotPacketData { }
